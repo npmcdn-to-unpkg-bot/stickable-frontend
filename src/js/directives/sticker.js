41020ecdd6c95de4d75bdd11b565c96578eeb2da
Anthony Kuske <anthonykuske@gmail.com>
2016-02-21 22:56:24 +0000

    Fix sticker directive

--- src/js/directives/sticker.js
+++ src/js/directives/sticker.js
@@ -3,7 +3,7 @@ app.directive('sticker', function factory() {
         restrict: 'C',
         link: function ($scope, element) {
             $scope.$watch("img", function (newValue, oldValue) {
-                if (newValue && newValue != oldValue) {
+                if (newValue) {
                     element.children('div').remove();
                     Sticker.init(element[0]);
                     element.find('.sticker-img').css('background-image', 'url(' + $scope.img + ')');
